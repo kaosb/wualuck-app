4af1f4ae111e9f27fac74a1f95305d634d60631e
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2014-03-20 00:12:29 -0300

    Wualuck app initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WualuckApp::Application.config.secret_key_base = 'bf9a739fc54fb3aa8a368e4851408e4c3716bfe27a64a262f2e1329a2b8fab1a66769d2994a27c99322dd318285546b14f878089adfa57e6e3fd086092db4fc2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WualuckApp::Application.config.session_store :cookie_store, key: '_wualuck-app_session'
