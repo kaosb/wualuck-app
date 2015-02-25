d8e89eb0bba8df231e52161c80d61fe44bae7731
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2015-02-25 10:42:23 -0300

    Front legacy

---
+++ wualuck-front/README.md
@@ -0,0 +1,4 @@
+wualuck_frontend
+===================
+
+Frontend, para Wualuck Sitio Publico, y Framework basado en boostrap3 para construir la interfaz del sitio publico y la aplicacion.
---
+++ wualuck-front/framework/front/js/script.js
@@ -0,0 +1,115 @@
+;(function($){
+
+	
+/* ---------------------------------------------------------------------
+ * Heights
+ * ---------------------------------------------------------------------
+ **/
+	$.fn.heights = function(args){
+		var defaults = {
+			itemClass : 'equal-height-item'
+		};
+		return $(this).each( function(){
+			var obj = this;
+			// Extend
+			if( $.isEmptyObject(args) === false )
+				$.extend( defaults, args );
+
+			obj.init = function(defaults, obj){
+				tallest = 0;
+				$(obj).find('.'+defaults.itemClass).each(function(){
+					$(this).css({ 'height': 'auto' });
+					if ( $(this).outerHeight() > tallest )
+						tallest = $(this).outerHeight();
+				});
+				$(obj).find('.'+defaults.itemClass).css({'height': tallest});
+			};
+
+			// Default Initialization
+			obj.init(defaults, obj);
+
+			// initilice with $(window) resize (resizeEnd)
+			$(window).load( function(){
+				obj.init(defaults, obj);
+			});
+
+			// initilice with $(window) resize (resizeEnd)
+			$(window).on( 'heightsResizeEnd', function(){
+				obj.init(defaults, obj);
+			});
+
+			// ResizeEnd event
+			$(window).resize(function(){
+				if( this.heightsResizeTo ) clearTimeout(this.heightsResizeTo);
+				this.heightsResizeTo = setTimeout(function(){
+					$(this).trigger('heightsResizeEnd');
+				}, 50);
+			});
+		});
+	};
+	$.wualack = {
+		heights : {
+			init : function( context ){
+				this.master( context );
+				this.widgets( context );
+			},
+			master : function( context ){
+				$('.equal-heights', context).heights();
+			},
+			widgets : function( context ){
+				$('.push-up, .widget-careers', context).heights({ 'itemClass' : 'widget-banner' });
+				$('.push-up, .widget-careers', context).heights({ 'itemClass' : 'widget-image-text' });
+				$('.push-up, .widget-careers', context).heights({ 'itemClass' : 'widget-weights' });
+			}
+		},
+	};
+
+	//
+
+})(jQuery);
+
+/**
+ * General Scripts
+ */
+jQuery(document).ready( function($){
+/*-------------------------------------- Fit vids */
+ 	$(".container").fitVids();
+ 	$(".container-fluid").fitVids();
+/*-------------------------------------- Heights */
+ 	$.wualack.heights.init();
+ /*-------------------------------------- Click Responsive Top Menu */
+	/* TOP MENU */
+	$('#nav-responsive').click( function(){
+		$('#menu-responsive').slideToggle();
+		$('#nav-responsive').toggleClass('active');
+	});	
+/*-------------------------------------- Stretch image */
+	$('img.wide-image').height( function(){
+		
+	});
+
+/*-------------------------------------- Stretch image */
+	$('.tooltip-categories').tooltip();
+
+
+/*-------------------------------------- Fixed Sidebar */
+	/*
+	if( $(document).width() <= 719 ){ 
+			//GET BROWSER WINDOW HEIGHT
+			var currHeight = $(window).height();
+			//SET HEIGHT OF SIDEBAR AND CONTENT ELEMENTS
+			$('#sidebar-fixed, #content-fixed').css('height', currHeight);
+
+			//ON RESIZE OF WINDOW
+			$(window).resize(function() {
+
+				//GET NEW HEIGHT
+				var currHeight = $(window).height();	
+				//RESIZE BOTH ELEMENTS TO NEW HEIGHT
+				$('#sidebar-fixed, #content-fixed').css('height', currHeight);
+
+			});
+		}
+	});
+	*/
+});
